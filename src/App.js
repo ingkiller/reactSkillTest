c997886f2928b8f66ae532bac816d400b078f014
Ricardo <ricardordguezrmrez@gmail.com>
2020-06-12 08:29:13 -0400

    -init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {} from 'react';
+
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+
+
+
+function App() {
+
+  return (
+    <div className="container-fluid">
+      <h1>React test</h1>
+    </div>
+  );
+}
+
+export default App;
